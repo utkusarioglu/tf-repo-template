608fe1e51022b04eb7fa77c114f7b9fe17c9d556
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-10-03 13:47:13 +0000

    Chore(scripts): Enhance git updater
    - Add `echo` statement in `git-utils.sh:22` for better DX.
    - Add new experimental script file for finalizing template update.
      This script is intended to make it easier to finalize the template
      update process for the dev.

--- scripts/git-utils.sh
+++ scripts/git-utils.sh
@@ -17,7 +17,10 @@ git_template_update_record() {
   else
     sed -i '/TEMPLATE_LAST_COMMIT_EPOCH/d' $record_target 
   fi
-  echo "TEMPLATE_LAST_COMMIT_EPOCH=$template_date_epoch # $template_date_human" >> $record_target
+  echo "TEMPLATE_LAST_COMMIT_EPOCH=$template_date_epoch # \
+    $template_date_human" $record_target
+  echo "Updated template record in '$record_target' with epoch: \
+    '$template_date_epoch'"
 }
 
 git_last_commit_utc_date() {
